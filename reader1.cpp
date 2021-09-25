619027d9f3e2b7830d78b439783fe6dc5c65dbbf
Agriel <91367332+AgrieDark@users.noreply.github.com>
2021-09-25 11:29:17 +0300

    Add files via upload

---
+++ reader1.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include "memory.h"
+#include "Offsets.h"
+
+using namespace hazedumper;
+using namespace netvars;
+using namespace signatures;
+
+PModule Base;
+memory mem;
+
+int getHealth()
+{
+	DWORD localPlayer = mem.Read<DWORD>(Base.dwBase + dwLocalPlayer);
+	int health = mem.Read<int>(localPlayer + m_iHealth);
+	return health;
+}
+
+int main()
+{
+	while(!mem.Attach("csgo.exe", PROCESS_ALL_ACCESS)){}
+	Base = mem.GetModule("client.dll");
+
+	while (true)
+	{
+		std::cout << "Health: " << getHealth() << "\n";
+		system("cls");
+		Sleep(1);
+	}
+}
